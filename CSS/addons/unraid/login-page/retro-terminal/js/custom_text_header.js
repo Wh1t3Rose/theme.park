279842e4278ab182e3d91c5499b94e9d22c5ebaa
Tyler Cox <4251482+Wh1t3Rose@users.noreply.github.com>
2021-04-02 13:05:19 -0400

    Update custom_text_header.js

--- CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -1,14 +1,14 @@
- 
-//Custom Text Header //
+
+ //Custom Text Header //
 
 // Needs to be inside <section id="login" class="shadow">
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre {text-align:center;font-size: 1vw;}>      ____
-____ __  __ ___ _  _    ___ _____ ___ ____ ____ ____ _  _  
+<div class="custom-text-header"> <pre>                     __                             ___       __  
+ ____ __  __ ___ _  _    ___ _____ ___ ____ ____ ____ _  _ 
 ( ___(  )(  / __( )/ )  / __(  _  / __(_  _( ___(_  _( \/ )
  )__) )(__)( (__ )  (   \__ \)(_)( (__ _)(_ )__)  )(  \  / 
-(__) (______\___(_)\_)  (___(_____\___(____(____)(__) (__)                                                                                                                                    
-                                                       </pre> </div>
+(__) (______\___(_)\_)  (___(_____\___(____(____)(__) (__)  
+                                                                 </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
